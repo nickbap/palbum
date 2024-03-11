efb406fa92be332c13d58b62ed28bd2b366f6a31
Nick Bautista <nbautista12@gmail.com>
2024-03-11 13:00:50 -0700

    Add repr to Image

--- palbum/models.py
+++ palbum/models.py
@@ -9,3 +9,6 @@ class Image(db.Model):
     added_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     name = db.Column(db.Text, unique=True, nullable=False)
     is_visible = db.Column(db.Boolean, nullable=False, default=True)
+
+    def __repr__(self):
+        return f"<Image {self.id} {self.name}>"
