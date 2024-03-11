b93d136ce41ed336a3de53a9daa6beb34efced7a
Nick Bautista <nbautista12@gmail.com>
2024-03-11 15:05:28 -0700

    Remove make_shell_context from run.py

--- run.py
+++ run.py
@@ -1,11 +1,3 @@
 from palbum import create_app
-from palbum import db
-from palbum.model_storage import ImageModelStorage
-from palbum.models import Image
 
 app = create_app()
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {"db": db, "Image": Image, "ImageModelStorage": ImageModelStorage}
